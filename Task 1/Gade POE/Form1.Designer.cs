a3053f27d590405755fdd4e1fe1e0e3a1d7eb68e
Pacman526 <55444098+Pacman526@users.noreply.github.com>
2019-10-06 11:08:54 +0200

    Task 1
    Task 1 without changes

---
+++ Task 1/Gade POE/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Gade_POE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblMap = new System.Windows.Forms.Label();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.rtbInfo = new System.Windows.Forms.RichTextBox();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // lblMap
+            // 
+            this.lblMap.AutoSize = true;
+            this.lblMap.Location = new System.Drawing.Point(54, 43);
+            this.lblMap.Name = "lblMap";
+            this.lblMap.Size = new System.Drawing.Size(0, 13);
+            this.lblMap.TabIndex = 0;
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Location = new System.Drawing.Point(421, 34);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(0, 13);
+            this.lblScore.TabIndex = 1;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(582, 160);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Location = new System.Drawing.Point(582, 206);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 3;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // rtbInfo
+            // 
+            this.rtbInfo.AccessibleRole = System.Windows.Forms.AccessibleRole.List;
+            this.rtbInfo.Location = new System.Drawing.Point(423, 72);
+            this.rtbInfo.Name = "rtbInfo";
+            this.rtbInfo.ReadOnly = true;
+            this.rtbInfo.Size = new System.Drawing.Size(149, 357);
+            this.rtbInfo.TabIndex = 4;
+            this.rtbInfo.Text = "";
+            // 
+            // Timer
+            // 
+            this.Timer.Interval = 1000;
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(736, 439);
+            this.Controls.Add(this.rtbInfo);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.lblMap);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblMap;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.RichTextBox rtbInfo;
+        private System.Windows.Forms.Timer Timer;
+    }
+}
+
---
+++ Task 1/Gade POE/Form1.cs
@@ -0,0 +1,400 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gade_POE
+{
+    public partial class Form1 : Form
+    {
+        GameEnigine gameEngine = new GameEnigine();
+        Map map = new Map(10);
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void Form1_Load(object sender, EventArgs e)
+        {
+            map.BattlefieldCreator();
+            lblMap.Text = map.PopulateMap();
+
+            gameEngine.GameLogic(map.units);
+
+            rtbInfo.Text = gameEngine.info;  
+        }
+
+        public class Unit
+        {
+            //CLASS VARIABLES 
+            public int xPos, yPos, Health, maxHealth, speed, attack, attackRange;
+            public int team;
+            public char symbol;
+            public bool combatCheck;
+            public string info;
+            public int count;
+            double smallestDist;
+
+            //CLASS CONSTRUCTOR
+            public Unit(int _xPos, int _yPos, int _health, int _speed, int _attack, int _attackRange, int _team, char _symbol, bool _combatCheck)
+            {
+                xPos = _xPos;
+                yPos = _yPos;
+                Health = _health;
+                speed = _speed;
+                attack = _attack;
+                attackRange = _attackRange;
+                team = _team;
+                symbol = _symbol;
+                combatCheck = _combatCheck;
+            }
+
+            //CLASS METHODS
+            public void MoveUnit(Unit u, Unit closestUnit)
+            {
+                if (u.xPos > closestUnit.xPos)
+                {
+                    u.xPos -= u.speed; 
+                }
+
+                if (u.xPos < closestUnit.xPos)
+                {
+                    u.xPos += u.speed;
+                }
+
+                if (u.yPos > closestUnit.yPos)
+                {
+                    u.yPos -= u.speed;
+                }
+
+                if (u.yPos < closestUnit.yPos)
+                {
+                    u.yPos += u.speed;
+                }
+            }
+
+            public void CombatHandler(Unit closestUnit, Unit u)
+            {
+                closestUnit.Health -= u.attack;
+            }
+
+            public bool RangeCheck(Unit closestUnit, Unit u )
+            {
+                if (u.attackRange <= smallestDist)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+            public Unit ClosestUnit(Unit[] units, int numUnits, Unit u)
+            {
+
+                double[] distance = new double[numUnits];
+                int counter = 0;
+                int Team1 = 0, Team2 = 0;
+                int temp =0, count = 0;
+
+                for (int k = 0; k < units.Length; k++)
+                {
+                    if (units[k].team == 0)
+                    {
+                        Team1 += 1;
+                    }
+                    else
+                    {
+                        Team2 += 1;
+                    }
+                    
+                }
+
+                if (u.team == 0)
+                {
+                    for (int j = 0; j < numUnits; j++)
+                    {
+                        if (units[counter].team != u.team)
+                        {
+                            distance[j] = Math.Sqrt((units[counter].xPos - u.xPos) * (units[counter].xPos - u.xPos) + (units[counter].yPos - u.yPos) * (units[counter].yPos - u.yPos));
+                            counter += 1;
+                        }
+                        else
+                        {
+                            counter += 1;
+                        }
+                    }
+                }
+
+                if (u.team == 1)
+                {
+                    for (int j = 0; j < numUnits; j++)
+                    {
+                        if (units[counter].team != u.team)
+                        {
+                            distance[j] = Math.Sqrt(((units[counter].xPos - u.xPos) * (units[counter].xPos - u.xPos)) + ((units[counter].yPos - u.yPos) * (units[counter].yPos - u.yPos)));
+                            counter += 1;
+                        }
+                        else
+                        {
+                            counter += 1;
+                        }
+
+                    }
+                }
+
+                smallestDist = distance.Min();
+
+                for (int j = 0; j < distance.Length; j++)
+                {
+                    if (distance[j] == smallestDist)
+                    {
+                        temp = j;
+                    }
+                }
+
+                if (u.team == 0)
+                {
+                    for (int m = 0; m < units.Length; m++)
+                    {
+                        if (units[m].team == 1)
+                        {
+                            count += 1;
+                        }
+
+                        if (count == temp)
+                        {
+                            temp = count;
+                        }
+                    }
+                }
+
+                if (u.team == 1)
+                {
+                    for (int m = 0; m < units.Length; m++)
+                    {
+                        if (units[m].team == 0)
+                        {
+                            count += 1;
+                        }
+
+                        if (count == temp)
+                        {
+                            temp = count;
+                        }
+                    }
+
+                }
+
+                return units[temp];
+            }
+
+            public  void Death(Unit[] units ,int i)
+            {
+                units = units.Where((source, index) => index != i).ToArray();
+            }
+
+            public  string ToString(Unit u, string unitType, Unit[] units, int i)
+            {
+                info = "";
+                if (unitType == "MeleeUnit")
+                {
+                    info += "MeleeUnit " + Convert.ToString(i + 1) + "\n" + "____________" + "\n" + "Hp : " + u.Health + "\n" + "Damage : " + u.attack + "\n" + "Team : " + (u.team +1)+ "\n" + "In Combat : " + u.combatCheck + "\n" + "Symbol: " + u.symbol; ;
+                    
+                }
+
+                if (unitType == "RangedUnit")
+                {
+                    info += "RangedUnit " + Convert.ToString(i + 1) + "\n" + "____________"+ "\n" + "Hp : " + u.Health + "\n" + "Damage : " + u.attack + "\n" + "Team : " + (u.team +1)+ "\n" + "In Combat : " + u.combatCheck + "\n" + "Symbol: " + u.symbol; ;
+                  
+                }
+
+                info = info + "\n" + "\n";
+                return info;
+            }
+
+        }
+
+        public class Map
+        { 
+            //CLASS VARIABLES
+            public char[,] map = new char[20, 20];
+            public int unitAmount;
+            public Unit[] units;
+
+            //CLASS CONSTRUCTOR
+            public Map(int _unitAmount)
+            {
+                unitAmount = _unitAmount;
+            }
+
+            //CLASS METHODS
+            public void BattlefieldCreator()
+            {
+                for (int i = 0; i < 20; i++)
+                {
+                    for (int k = 0; k < 20; k++)
+                    {
+                        map[i, k] = Convert.ToChar("~");  
+                    }
+                }
+
+                Random rnd = new Random();
+                units = new Unit[unitAmount];
+
+                for (int i = 0; i < unitAmount; i++)
+                {
+                   
+                    int x = rnd.Next(0, 20);
+                    int y = rnd.Next(0, 20);
+                    int team = rnd.Next(0, 2);
+                    int unit = rnd.Next(0, 2);
+                    
+
+
+                    if (unit == 1 & team == 0)
+                    {
+                        Unit RangedUnit = new Unit(x, y, 10, 1, 1, 5, team , Convert.ToChar("R"), false);
+                        map[x, y] = RangedUnit.symbol;
+                        units[i] = RangedUnit;
+                    }
+
+                    if (unit == 1 & team == 1)
+                    {
+                        Unit RangedUnit = new Unit(x, y, 10, 1, 1, 5, team, Convert.ToChar("r"), false);
+                        map[x, y] = RangedUnit.symbol;
+                        units[i] = RangedUnit;
+                    }
+
+                    if (unit == 0 & team == 0)
+                    {
+                        Unit MeleeUnit = new Unit(x, y, 20, 2, 2, 1, team, Convert.ToChar("M"), false);
+                        map[x, y] = MeleeUnit.symbol;
+                        units[i] = MeleeUnit;
+                    }
+
+                    if (unit == 0 & team == 1)
+                    {
+                        Unit MeleeUnit = new Unit(x, y, 20, 2, 2, 1, team, Convert.ToChar("m"), false);
+                        map[x, y] = MeleeUnit.symbol;
+                        units[i] = MeleeUnit;
+                        
+                    }
+                }
+            }
+
+            public string PopulateMap()
+            {
+                string mapLayout = "";
+
+                for (int i = 0; i < 20; i++)
+                {
+                    for (int k = 0; k < 20; k++)
+                    {
+                        mapLayout += map[i,k]; 
+                    }
+                    mapLayout = mapLayout + "\n";
+                }
+
+                return mapLayout;
+            }
+
+            public void UpdatePosition(int i, int oldX, int oldY)
+            {
+                
+                map[units[i].xPos, units[i].yPos] = units[i].symbol;
+                map[oldX, oldY] = Convert.ToChar("~");
+                PopulateMap();
+            }
+
+        }
+        class GameEnigine
+        {
+            public int roundCheck;
+            public string info;
+            public string unitType;
+            Unit closestUnit;
+            public int x, y, i;
+            
+                
+                public void GameLogic(Unit[] units)
+                {     
+                    
+                    info = "";
+                
+                    for (i = 0; i < units.Length; i++)
+                    {
+                        Unit u = (Unit)units[i];
+
+                        x = u.xPos;
+                        y = u.yPos;
+
+                        if (u.attackRange == 5)
+                        {
+                            unitType = "RangedUnit";
+                        }
+
+                        if (u.attackRange == 1)
+                        {
+                            unitType = "MeleeUnit";
+                        }
+                    
+                        info += u.ToString(u, unitType, units, i);
+
+                        if (units[i].Health <= 0)
+                        {
+                            u.Death(units, i);
+                        }
+                        else
+                        {
+                            closestUnit = u.ClosestUnit(units, units.Length, u);
+                            if (u.RangeCheck(closestUnit, u) == true)
+                            {
+                                u.combatCheck = true;
+                                u.CombatHandler(closestUnit, u);
+                            }
+                            else
+                            {
+                                u.MoveUnit(u, closestUnit);
+                                
+                            }
+                        }
+                        
+                         
+                    }
+                }
+                
+        }
+
+            
+            
+
+            private void btnStart_Click(object sender, EventArgs e)
+            {
+                Timer.Enabled = true;
+                Timer.Start();
+            }
+
+            private void Timer_Tick(object sender, EventArgs e)
+            {
+                
+                rtbInfo.Clear();
+                gameEngine.GameLogic(map.units);
+                rtbInfo.Text = gameEngine.info;       
+            }
+
+            private void btnPause_Click(object sender, EventArgs e)
+            {
+                Timer.Enabled = false;
+                Timer.Stop();
+            }
+    }
+}
